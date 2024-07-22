43bc06a69623be43c933a9dd6c15c22a7ae7fe17
JoaoMB1337 <joaobarbosa152002@gmail.com>
2024-07-22 14:16:19 +0100

    Feat: new tecnology

---
+++ app/src/main/java/com/example/innodriveapp/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.innodriveapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private ValueCallback<Uri[]> uploadMessage;
+    private static final int FILECHOOSER_RESULTCODE = 1;
+    private static final int IMAGE_CHOOSER_REQUEST = 2;
+    private String cameraFilePath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(null);
+                }
+                uploadMessage = filePathCallback;
+
+                String[] acceptTypes = fileChooserParams.getAcceptTypes();
+                boolean isCamera = fileChooserParams.isCaptureEnabled() || (acceptTypes.length > 0 && "image/*".equals(acceptTypes[0]));
+
+                if (isCamera) {
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                        File photoFile = null;
+                        try {
+                            photoFile = createImageFile();
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                        if (photoFile != null) {
+                            Uri photoURI = FileProvider.getUriForFile(MainActivity.this, "com.example.innodriveapp.fileprovider", photoFile);
+                            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                            startActivityForResult(takePictureIntent, FILECHOOSER_RESULTCODE);
+                        }
+                    }
+                } else {
+                    Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                    contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                    contentSelectionIntent.setType("image/*");
+                    startActivityForResult(contentSelectionIntent, IMAGE_CHOOSER_REQUEST);
+                }
+                return true;
+            }
+        });
+
+        webView.loadUrl("http://192.168.2.11:8000/");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((requestCode == FILECHOOSER_RESULTCODE || requestCode == IMAGE_CHOOSER_REQUEST) && resultCode == RESULT_OK) {
+            Uri result = null;
+            if (data != null && data.getData() != null) {
+                result = data.getData();
+            } else if (requestCode == FILECHOOSER_RESULTCODE && cameraFilePath != null) {
+                result = Uri.parse(cameraFilePath);
+            }
+            if (uploadMessage != null) {
+                uploadMessage.onReceiveValue(new Uri[]{result});
+                uploadMessage = null;
+            }
+        } else {
+            if (uploadMessage != null) {
+                uploadMessage.onReceiveValue(null);
+                uploadMessage = null;
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(null);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        cameraFilePath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+}
